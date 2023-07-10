3e81e89a13a679ca0048fcc34c155c915bfc6692
OlgaRoenko41 <137801715+OlgaRoenko41@users.noreply.github.com>
2023-07-10 22:35:23 +0300

    Add files via upload

---
+++ lvl_1/complete_task_1.1.py
@@ -0,0 +1,5 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print('1.',my_favorite_songs[0:14])
+print('2.',my_favorite_songs[64:])
+print('3.',my_favorite_songs[16:30])
+print('4.',my_favorite_songs[-26:-15])
