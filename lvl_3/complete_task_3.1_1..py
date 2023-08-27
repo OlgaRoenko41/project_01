77f243a3da45164054a69a630b3284f830811d27
OlgaRoenko41 <137801715+OlgaRoenko41@users.noreply.github.com>
2023-08-27 17:41:35 +0300

    Add files via upload

---
+++ lvl_3/complete_task_3.1_1..py
@@ -0,0 +1,55 @@
+# Задача 3.1.
+# Создайте класс матрицы (или таблицы).
+# Требования к классу:
+#   - каждая колонка является числом от 1 до n (n любое число, которые вы поставите!)
+#   - в каждой ячейке содержится либо число, либо None
+#   - доступы следующие методы матрицы:
+#       * принимать новые значения, 
+#       * заменять существующие значения, 
+#       * выводить число строк и колонок.
+
+# Пример матрицы 10 на 10 из единиц:
+# [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+# Примечание! 
+#   - новый класс не запрещено строить на базе существующих типов данных: списков, словарей и тд.
+#   - отображать в таблице/матрице название колонки не обязательно!
+#   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
+#   - проявите фантазию :)
+
+class Matrix:
+    def __init__(self, value, rows, cols):
+        self.field = [[value for _ in range(cols)] for _ in range(rows)]
+        self.rows = rows
+        self.cols = cols
+        self.value = value
+
+    def get_value(self, row, col):
+        if row in range(self.rows) and col in range(self.cols):
+            return self.field[row][col]
+
+    def set_value(self, row, col, value):
+        self.field[row][col] = value
+
+    def n_rows(self):
+        return self.rows
+
+    def n_cols(self):
+        return self.cols
+
+
+table = Matrix(9, 5, 5)
+table.set_value(1, 2, 3)
+for i in range(table.n_rows()):
+    for j in range(table.n_cols()):
+        print(table.get_value(i, j), end=' ')
+    print()
